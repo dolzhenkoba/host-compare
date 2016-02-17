3fd01d443bf657444771d9e1fbb9dafb4db74db5
dolzhenkoba <dasflancer@gmail.com>
2016-02-17 11:56:59 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+;(function($){
+
+
+ $('.carousel').carousel({
+  interval: 200000,
+  pause: 'hover',
+  wrap: true
+  });
+
+  $('.carousel-bottom').carousel({
+  interval: 2000,
+  pause: 'hover',
+  wrap: true
+  });
+
+})(jQuery);
